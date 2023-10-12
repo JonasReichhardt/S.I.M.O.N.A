73e6641a80988b9d91a112ad3b31378cd3ddcc82
Jonas Reichhardt <j.reichhardt@tig.at>
2023-10-12 20:51:29 +0200

    implement run script

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+bold="\e[1m"
+uline="\e[4m"
+reset="\e[0m"
+green="\e[0;92m"
+blue="\e[0;94m"
+
+echo -e "${blue}${bold}${uline}[S.I.M.O.N.A | Starting application]${reset}"
+echo " "
+
+echo -e "${green}${bold}[Building frontend application]${reset}"
+echo -e "${green}Installing dependencies...${reset}"
+cd src/frontend
+npm install
+echo -e "${green}Building application...${reset}"
+npm run build
+echo " "
+
+echo -e "${green}${bold}[Building backend application]${reset}"
+echo -e "${green}Installing dependencies...${reset}"
+cd ../backend
+npm install
+echo -e "${green}Starting application...${reset}"
+npm run start
+cd ../..
+echo " "
