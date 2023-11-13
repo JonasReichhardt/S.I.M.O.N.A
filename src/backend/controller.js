823de03bdc6d21216beeb8b4a4fcfd4736b65b55
Jonas Reichhardt <j.reichhardt@tig.at>
2023-11-13 17:28:40 +0100

    dev controller

--- src/backend/controller.js
+++ src/backend/controller.js
@@ -4,17 +4,28 @@ const SLEEP = 5
 const DIR = 6
 const STEP = 13
 
-const sleep_pin = new Gpio.Gpio(SLEEP, { mode: Gpio.OUTPUT, pullUpDown: Gpio.PUD_DOWN });
+const sleep_pin = new Gpio.Gpio(SLEEP, { mode: Gpio.OUTPUT, pullUpDown: Gpio.Gpio.PUD_DOWN });
 const dir_pin = new Gpio.Gpio(DIR, { mode: Gpio.OUTPUT });
 const step_pin = new Gpio.Gpio(STEP, { mode: Gpio.OUTPUT });
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) { break; }
+    }
+}
 
 sleep_pin.digitalWrite(1)
 dir_pin.digitalWrite(1)
 
-setInterval(()=>{
-    for(var i = 0;i<200;i++){
-        step_pin.trigger(100,1)
-    }
-},5000)
+for(var i = 0;i<200;i++){
+    step_pin.digitalWrite(1)
+    console.log("stepping")
+    sleep(10)
+    step_pin.digitalWrite(0)
+    sleep(10)
+}
+
+sleep_pin.digitalWrite(0)
 
 
