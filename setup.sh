0db71c33e9a4263b0751112f405deafe5bcf7a1a
JonasReichhardt <reichhardt.jonas@gmail.com>
2024-05-17 17:59:42 +0200

    fix typo

--- setup.sh
+++ setup.sh
@@ -12,14 +12,15 @@ echo " "
 echo -e "${green}${bold}[Installing OS dependencies]${reset}"
 echo " "
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 echo -e "${green}Installing WM8960 driver...${reset}"
 echo " "
+cd .
 git clone https://github.com/waveshare/WM8960-Audio-HAT
 cd WM8960-Audio-HAT
-sudo ./install.sh 
+sudo ./install.sh
 
 echo -e "${green}Installing audio player...${reset}"
 sudo apt-get install mpg321 -y
