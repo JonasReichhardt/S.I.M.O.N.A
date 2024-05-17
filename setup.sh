312290b5d06e5ed4de5a319d34fdbcf649fa901b
JonasReichhardt <reichhardt.jonas@gmail.com>
2024-05-17 17:35:06 +0200

    add waveshare driver installation

--- setup.sh
+++ setup.sh
@@ -12,6 +12,15 @@ echo " "
 echo -e "${green}${bold}[Installing OS dependencies]${reset}"
 echo " "
 
+sudo apt-get update
+sudo apt-get upgrade
+
+echo -e "${green}Installing WM8960 driver...${reset}"
+echo " "
+git clone https://github.com/waveshare/WM8960-Audio-HAT
+cd WM8960-Audio-HAT
+sudo ./install.sh 
+
 echo -e "${green}Installing audio player...${reset}"
 sudo apt-get install mpg321 -y
 echo " "
@@ -37,5 +46,8 @@ echo " "
 echo -e "${green}npm${reset}"
 npm -v
 echo " "
+sudo dkms status
+echo " "
 
-
+read -p "Press any key to reboot"
+sudo reboot
